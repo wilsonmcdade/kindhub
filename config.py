8a97536bb4fff83100c803ff0a36b758ccdf88da
Wilson <wilson.mcdade@gmail.com>
2020-01-22 12:17:12 -0500

    minor changes to config.py, added a main function for testing purposes

--- config.py
+++ config.py
@@ -3,36 +3,38 @@ Config parser for kindhub
 Author: Wilson McDade
 """
 
-from app import Widget
+from dataclass import Widget
 
 def find_next_widget(line,file):
-    
+
     for line in file:
         if line != "\n":
             pass
         else:
-            parser(line,file)
+            return parser(line,file)
 
 def gather_config(line,file):
-    
+
     config = []
     config.append(line)
-    
+
     for line in file:
         if line != "\n":
             return config
-        elif line[0] == #
+        elif line[0] == "#":
             pass
         else:
             config.append(line.split())
-    
+
 def parser(line, file):
-    
+
     name = ""
     strname = ""
+    route = ""
+    config = ""
 
     for line in file:
-        
+
         if line[0] == "#":
             pass
         elif line[0] == "\n":
@@ -40,7 +42,7 @@ def parser(line, file):
             pass
 
         line_split = line.split()
-        
+
         if line_split[0][0] == "[":
             # line is the name of the widget
             name = line_split[0][1:-1]
@@ -54,10 +56,10 @@ def parser(line, file):
                 print("Widget "+name+" is not enabled")
                 next = find_next_widget(line,file)
         elif line_split[0] == "strname":
-            strname = line[1]
+            strname = line[9:].strip()
         elif line_split[0] == "route":
             route = line_split[1]
-        else: 
+        else:
             config = gather_config(line,file)
     return Widget(name=name,route=route,strname=strname,config=config,next=find_next_widget(line,file))
 
@@ -68,3 +70,6 @@ def config_reader(filename):
                 pass
             else:
                 parser(line,file)
+
+if __name__ == "__main__":
+    print(config_reader("config.conf"))
