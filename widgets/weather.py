c08d7d965b14d8b6dac55a9a4d068ad459efba80
Wilson <wilson.mcdade@gmail.com>
2020-02-10 15:39:14 -0500

    Fixed customization in weather widget

--- widgets/weather.py
+++ widgets/weather.py
@@ -5,7 +5,15 @@ Author: Wilson McDade
 
 from __main__ import app, enabled_widgets
 from flask import render_template
+from geopy.geocoders import Nominatim
 
 @app.route('/weather')
 def weather():
-    return render_template('weather.html',widgets = enabled_widgets)
+    geolocator = Nominatim(user_agent="kindhub")
+    given_loc = enabled_widgets['weather']['loc']
+    if len(given_loc) > 1:
+        location = geolocator.geocode(given_loc)
+        long = location.longitude
+        lat = location.latitude
+        loc_coord = str(lat)[:7] + "," + str(long)[:7]
+    return render_template('weather.html',widgets = enabled_widgets, loc = loc_coord, loc_name = given_loc)
